d69d2f286467ee765497483c36a17ac0e0823a82
Putra Pratama <pratamabusiness@gmail.com>
2023-04-03 11:07:37 +0800

    add motion framer @ header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,28 @@
 import React from "react";
 import { SocialIcon } from "react-social-icons";
 SocialIcon;
+import { motion } from "framer-motion";
 
 type Props = {};
 
 function Header({}: Props) {
   return (
     <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
-      <div className="flex flex-row items-center">
+      <motion.div
+      initial={{
+        x: -500,
+        opacity: 0,
+        scale: 0.5
+      }}
+      animate={{
+        x: 0,
+        opacity: 1,
+        scale: 1
+      }}
+      transition={{
+        duration: 0.8
+      }}
+      className="flex flex-row items-center">
         {/* social icons*/}
         <SocialIcon
           url='https://instagram.com/rzkiypratama'
@@ -25,13 +40,27 @@ function Header({}: Props) {
           bgColor='transparent'
         />
         <SocialIcon
-          url='https://github.com/rzkiyprtm'
+          url='https://github.com/rzkiypratama'
           fgColor='gray'
           bgColor='transparent'
         />
-      </div>
+      </motion.div>
 
-      <div className="flex flex-row items-center text-gray-300 cursor-pointer">
+      <motion.div 
+      initial={{
+        x: 500,
+        opacity: 0,
+        scale: 0.5
+      }}
+      animate={{
+        x: 0,
+        opacity: 1,
+        scale: 1
+      }}
+      transition={{
+        duration: 0.8
+      }}
+      className="flex flex-row items-center text-gray-300 cursor-pointer">
         {/* email icon */}
         <SocialIcon
         className="cursor-pointer"
@@ -40,7 +69,7 @@ function Header({}: Props) {
         bgColor="transparent"
         />
         <p className="uppercase hidden md:inline-flex text-sm text-gray-400">Get in touch</p>
-      </div>
+      </motion.div>
     </header>
   );
 }
