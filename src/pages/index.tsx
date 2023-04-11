d554af766fb6a21eb99c15ff9695c25ad6d30bf7
Putra Pratama <pratamabusiness@gmail.com>
2023-04-11 23:06:27 +0800

    feat: add projects section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import WorkExperience from "@/components/WorkExperience";
 import Skills from "@/components/Skills";
+import Projects from "@/components/Projects";
 Skills
 
 Header;
@@ -42,13 +43,15 @@ const Home: NextPage = () => {
       <WorkExperience/>
       </section>
 
-      {/* skills */}
-      <section id="experience" className="snap-start">
+      <section id="skills" className="snap-start">
         <Skills/>
       </section>
 
       {/* projects */}
-
+      <section id="projects" className="snap-start">
+      <Projects/>
+      </section>
+      
       {/* contact me */}
     </div>
   );
