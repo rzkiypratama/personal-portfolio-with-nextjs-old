be99ccb7754359db029b8d1e418a0a17c3b0bbf4
Putra Pratama <pratamabusiness@gmail.com>
2023-04-15 22:54:38 +0800

    fix: fix sizing in z index

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,22 +7,23 @@ type Props = {};
 
 function Header({}: Props) {
   return (
-    <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
+    <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
       <motion.div
-      initial={{
-        x: -500,
-        opacity: 0,
-        scale: 0.5
-      }}
-      animate={{
-        x: 0,
-        opacity: 1,
-        scale: 1
-      }}
-      transition={{
-        duration: 0.8
-      }}
-      className="flex flex-row items-center">
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 0.8,
+        }}
+        className='flex flex-row items-center'
+      >
         {/* social icons*/}
         <SocialIcon
           url='https://instagram.com/rzkiypratama'
@@ -46,29 +47,32 @@ function Header({}: Props) {
         />
       </motion.div>
 
-      <motion.div 
-      initial={{
-        x: 500,
-        opacity: 0,
-        scale: 0.5
-      }}
-      animate={{
-        x: 0,
-        opacity: 1,
-        scale: 1
-      }}
-      transition={{
-        duration: 0.8
-      }}
-      className="flex flex-row items-center text-gray-300 cursor-pointer">
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 0.8,
+        }}
+        className='flex flex-row items-center text-gray-300 cursor-pointer'
+      >
         {/* email icon */}
         <SocialIcon
-        className="cursor-pointer"
-        network="email"
-        fgColor="gray"
-        bgColor="transparent"
+          className='cursor-pointer'
+          network='email'
+          fgColor='gray'
+          bgColor='transparent'
         />
-        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">Get in touch</p>
+        <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>
+          Get in touch
+        </p>
       </motion.div>
     </header>
   );
