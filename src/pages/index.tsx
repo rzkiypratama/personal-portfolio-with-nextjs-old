3ab6ff8f8e86ed37011525c604c41acda133a8a3
Putra Pratama <pratamabusiness@gmail.com>
2023-04-21 09:31:14 +0800

    fix: fix styling for half screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import WorkExperience from "@/components/WorkExperience";
 import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
 import ContactMe from "@/components/ContactMe";
+import Link from "next/link";
 Skills;
 
 Header;
@@ -14,37 +15,32 @@ Hero;
 
 const Home: NextPage = () => {
   return (
-    <div className='bg-[rgb(36,36,36)] text-white h-scree snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-{#F7AB0A}/80'>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-{#F7AB0A}/80'>
       <Head>
         <title>Putra's Posrtfolio</title>
       </Head>
 
-      <section>
-        <Header />
-      </section>
+      <Header />
 
-      <section
-        id='hero'
-        className='snap-start max-w-7xl mx-auto'
-      >
+      <section id='hero' className='snap-center'>
         <Hero />
       </section>
 
-      <section
-        id='about'
-        className='snap-center max-w-7xl mx-auto'
-      >
+      <section id='about' className='snap-center'>
         <About />
       </section>
 
       <section
         id='experience'
-        className='snap-center max-w-7xl mx-auto'
+        className='snap-center'
       >
         <WorkExperience />
       </section>
 
-      <section id='skills' className='snap-start'>
+      <section
+        id='skills'
+        className='snap-center'
+      >
         <Skills />
       </section>
 
@@ -55,9 +51,24 @@ const Home: NextPage = () => {
         <Projects />
       </section>
 
-      <section id="contact" className="snap-start">
+      <section
+        id='contact'
+        className='snap-start'
+      >
         <ContactMe />
       </section>
+
+      <Link href='#hero'>
+        <footer className='sticky bottom-5 w-full cursor-pointer'>
+          <div className='flex items-center justify-center'>
+            <img
+              className='h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer'
+              src='https://media.licdn.com/dms/image/D5603AQFahoCGJVPcTw/profile-displayphoto-shrink_400_400/0/1679397025923?e=1686182400&v=beta&t=79gYoL8xREV342zltonX98pJtdqksstJTwVVosm6zFQ'
+              alt=''
+            />
+          </div>
+        </footer>
+      </Link>
     </div>
   );
 };
