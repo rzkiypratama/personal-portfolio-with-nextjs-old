7e1b38ef9152a51e78df64cf574c2ab275a2570f
Putra Pratama <pratamabusiness@gmail.com>
2023-04-24 14:39:46 +0800

    fix: styling scrollbar error? say no more

--- src/components/WorkExperience.tsx
+++ src/components/WorkExperience.tsx
@@ -14,11 +14,11 @@ const WorkExperience = (props: Props) => {
       transition={{ duration: 1.3 }}
       className='h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center'
     >
-      <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500-500 text-2xl'>
+      <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
         Experience
       </h3>
 
-      <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-{#F7AB0A}/80'>
+      <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
         <ExperienceCard />
         <ExperienceCard />
         <ExperienceCard />
