f0229ac3c9229c4d35038644e6c0921601c3302e
Putra Pratama <pratamabusiness@gmail.com>
2023-05-03 11:17:38 +0800

    feature: add anchor new tab for project link

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -30,10 +30,16 @@ const Projects = ({}: Props) => {
           <div className='space-y-10 px-0 md:px-10 max-w-6xl'>
             <h4 className='text-xl lg:text-4xl md:text-4xl font-semibold text-center'>
               <span className='underline decoration-[#F7AB0A]/50'>
-                Case Study 1 of{" "}
-                4:
+                Case Study 1 of 4:
               </span>{" "}
-              Monlight Cafe
+              <a
+                target='_blank'
+                href='https://monlight.vercel.app/'
+                rel='noopener noreferrer'
+                className='cursor-pointer'
+              >
+                Monlight Cafe
+              </a>
             </h4>
 
             {/* <div className='flex items-center space-x-2 justify-center'>
@@ -75,10 +81,16 @@ const Projects = ({}: Props) => {
           <div className='space-y-10 px-0 md:px-10 max-w-6xl'>
             <h4 className='text-xl lg:text-4xl md:text-4xl font-semibold text-center'>
               <span className='underline decoration-[#F7AB0A]/50'>
-                Case Study 2 of{" "}
-                4:
+                Case Study 2 of 4:
               </span>{" "}
-              E-wallet B-Pay
+              <a
+                target='_blank'
+                href='https://monlight-wallet.vercel.app/'
+                rel='noopener noreferrer'
+                className='cursor-pointer'
+              >
+                E-wallet - Fazzpay
+              </a>
             </h4>
 
             {/* <div className='flex items-center space-x-2 justify-center'>
@@ -90,14 +102,17 @@ const Projects = ({}: Props) => {
             </div> */}
 
             <p className='text-lg text-center md:text-left'>
-            Digital wallet application that you can make transfers and receive transfers, search features, profiles etc like gop**, dan*, etc!
+              Digital wallet application that you
+              can make transfers and receive
+              transfers, search features, profiles
+              etc like gop**, dan*, etc!
             </p>
           </div>
         </motion.div>
 
-          {/* project 3 */}
+        {/* project 3 */}
 
-          <motion.div
+        <motion.div
           initial={{ y: -200, opacity: 0 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 1.3 }}
@@ -114,10 +129,16 @@ const Projects = ({}: Props) => {
           <div className='space-y-10 px-0 md:px-10 max-w-6xl'>
             <h4 className='text-xl lg:text-4xl md:text-4xl font-semibold text-center'>
               <span className='underline decoration-[#F7AB0A]/50'>
-                Case Study 3 of{" "}
-                4:
+                Case Study 3 of 4:
               </span>{" "}
+              <a
+                target='_blank'
+                href='https://rima-project.vercel.app/'
+                rel='noopener noreferrer'
+                className='cursor-pointer'
+              >
               RIMA Furniture E-commerce
+              </a>
             </h4>
 
             {/* <div className='flex items-center space-x-2 justify-center'>
@@ -129,14 +150,20 @@ const Projects = ({}: Props) => {
             </div> */}
 
             <p className='text-lg text-center md:text-left'>
-            Website for furniture ecommerce, there are many features likes ecommerce in general. User can upload their products to sell it, while other users can place orders to bought it, search products and edit their profile too.
+              Website for furniture ecommerce,
+              there are many features likes
+              ecommerce in general. User can
+              upload their products to sell it,
+              while other users can place orders
+              to bought it, search products and
+              edit their profile too.
             </p>
           </div>
         </motion.div>
 
-         {/* project 4 */}
+        {/* project 4 */}
 
-         <motion.div
+        <motion.div
           initial={{ y: -200, opacity: 0 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 1.3 }}
@@ -153,10 +180,16 @@ const Projects = ({}: Props) => {
           <div className='space-y-10 px-0 md:px-10 max-w-6xl'>
             <h4 className='text-xl lg:text-4xl md:text-4xl font-semibold text-center'>
               <span className='underline decoration-[#F7AB0A]/50'>
-                Case Study 4 of{" "}
-                4:
+                Case Study 4 of 4:
               </span>{" "}
-              Movie Ticketing - Tickits
+              <a
+                target='_blank'
+                href='https://lepisa-fe.vercel.app/'
+                rel='noopener noreferrer'
+                className='cursor-pointer'
+              >
+                Movie Ticketing - Tickits
+              </a>
             </h4>
 
             {/* <div className='flex items-center space-x-2 justify-center'>
@@ -168,12 +201,17 @@ const Projects = ({}: Props) => {
             </div> */}
 
             <p className='text-lg text-center md:text-left'>
-            Have you ever try booking movie tickets through online? Try this! Cinema ticket booking application. As user you can search, order, edit your profile, etc. There is an admin feature that can add movies and schedules.
+              Have you ever try booking movie
+              tickets through online? Try this!
+              Cinema ticket booking application.
+              As user you can search, order, edit
+              your profile, etc. There is an admin
+              feature that can add movies and
+              schedules.
             </p>
           </div>
         </motion.div>
-        
-        
+
         {/* projects */}
         {/* projects */}
         {/* projects */}
