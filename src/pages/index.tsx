35d50b24e3b3d082da5c04700de774a6c8ddb002
Putra Pratama <pratamabusiness@gmail.com>
2023-05-26 16:51:13 +0800

    feat: add ssg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -68,3 +68,13 @@ const Home = (props: Props) => {
 };
 
 export default Home;
+
+export async function getStaticProps() {
+  // Tidak ada pemanggilan API atau pengambilan data eksternal
+
+  // Mengembalikan objek props kosong
+  return {
+    props: {},
+    revalidate: 3600 // Menentukan interval pembuatan ulang halaman dalam detik (misalnya, 1 jam)
+  };
+}
