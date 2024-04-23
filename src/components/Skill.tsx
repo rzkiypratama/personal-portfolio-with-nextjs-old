5076b7c82f70800553419b0ac7905a70bbd7385e
putra pratama <107741831+rzkiypratama@users.noreply.github.com>
2024-04-23 18:54:54 +0800

    Update Skill.tsx

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -27,7 +27,7 @@ function Skill({ percent, src }: Props) {
       <div className="absolute z-0 h-24 w-24 rounded-full opacity-0 transition duration-300 ease-in-out group-hover:bg-white group-hover:opacity-80 md:h-28 md:w-28 xl:h-32 xl:w-32">
         <div className="flex h-full items-center justify-center">
           <p className="text-3xl font-bold text-black opacity-100">
-            // {percent}%
+  
           </p>
         </div>
       </div>
